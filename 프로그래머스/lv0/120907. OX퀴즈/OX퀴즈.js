df5a0200c462b6cf623b300680c2786e76212a44
Woosung Lim <wjdxor1224@gmail.com>
2023-07-01 23:16:13 +0900

    [level 0] Title: OX퀴즈, Time: 0.12 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
+++ 프로그래머스/lv0/120907. OX퀴즈/OX퀴즈.js
@@ -1,16 +1,8 @@
 function solution(quiz) {
     var answer = [];
     for(item of quiz) {
-        const formula = item.split('=');
-        const left =  formula[0].trim().split(' ');
-        const right = formula[1];
-        
-        if(left.includes('-')) {
-            answer.push(Number(left[0]) - Number(left[2]) === Number(right) ? 'O' : 'X');
-        } else {
-            answer.push(Number(left[0]) + Number(left[2]) === Number(right) ? 'O' : 'X');   
-        }
-        
+        const [left, right] = item.split('=');
+        answer.push(eval(left) === Number(right) ? 'O' : 'X');
     }
     return answer;
 }
