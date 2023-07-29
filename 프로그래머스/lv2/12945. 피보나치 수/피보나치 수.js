6f0c41554fa4634bed4d4311bcc61ffecabe176e
Woosung Lim <wjdxor1224@gmail.com>
2023-07-29 20:49:12 +0900

    [level 2] Title: 피보나치 수, Time: 4.42 ms, Memory: 38.4 MB -BaekjoonHub

--- 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
+++ 프로그래머스/lv2/12945. 피보나치 수/피보나치 수.js
@@ -3,7 +3,7 @@ function solution(n) {
     const arr = new Array(n).fill(0);
     
     arr[1] = 1;
-    arr[2] = 1;
+    // arr[2] = 1;
     
     for(let i = 2; i <= n; i++) {
         arr[i] = (arr[i - 1] + arr[i - 2]) % MAX;
