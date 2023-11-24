d263e886c86fffe7f0b337e4218e8680b33eb124
Woosung Lim <wjdxor1224@gmail.com>
2023-11-24 14:13:15 +0900

    [level 2] Title: 할인 행사, Time: 0.08 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/2/131127. 할인 행사/할인 행사.js
+++ 프로그래머스/2/131127. 할인 행사/할인 행사.js
@@ -1,19 +1,16 @@
 function solution(want, number, discount) {
     let count = 0;
+    for (let i = 0; i < discount.length - 9; i++) {
+        const slice = discount.slice(i, i + 10);
 
-    for (let i = 0; i < discount.length - 9; i += 1) {
-        const list = discount.slice(i, i + 10);
-
-        for (j = 0; j < want.length; j += 1) {
-            if (list.filter(v => v === want[j]).length !== number[j]) {
+        let flag = true;
+        for (let j = 0; j < want.length; j++) {
+            if (slice.filter(item => item === want[j]).length !== number[j]) {
+                flag = false;
                 break;
             }
         }
-
-        if (j === want.length) {
-            count += 1;
-        }
-     }
-    
+        if (flag) count += 1;
+    }
     return count;
 }
