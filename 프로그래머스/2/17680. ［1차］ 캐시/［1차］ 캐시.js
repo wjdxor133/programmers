2e23330bfedd845e9c4cfa4be2dc0bc6a4d2bec8
Woosung Lim <wjdxor1224@gmail.com>
2023-12-09 17:53:57 +0900

    [level 2] Title: [1차] 캐시, Time: 12.17 ms, Memory: 36.5 MB -BaekjoonHub

--- 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
+++ 프로그래머스/2/17680. ［1차］ 캐시/［1차］ 캐시.js
@@ -1,17 +1,18 @@
 function solution(cacheSize, cities) {
     let memory = [];
     let time = 0;
+    const MISS = 5, HIT = 1;
     cities = cities.map(city => city.toLowerCase());
     
     for(let i = 0; i < cities.length; i++) {
         if(!memory.includes(cities[i])) {
             memory = [cities[i], ...memory];
-            time += 5;
+            time += MISS;
             if(memory.length > cacheSize) memory.pop();
         } else {
             memory = memory.filter(item => item !== cities[i]);
             memory = [cities[i], ...memory];
-            time += 1;
+            time += HIT;
         }
     }
     
