914661fcfcdfc3783a7bf0e969e93085cddebe82
Woosung Lim <wjdxor1224@gmail.com>
2023-12-15 19:15:45 +0900

    [level unrated] Title: 각도기, Time: 0.03 ms, Memory: 33.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120829. 각도기/각도기.js
@@ -0,0 +1,10 @@
+function solution(angle) {
+    if (angle < 90) 
+        return 1;
+    if (angle === 90) 
+        return 2;
+    if (angle > 90 && angle < 180) 
+        return 3;
+    if (angle === 180)
+        return 4;
+}
