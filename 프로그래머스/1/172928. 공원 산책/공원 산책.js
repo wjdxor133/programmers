adcef6b1fc78f73ef44026a66057b786ed652e1f
Woosung Lim <wjdxor1224@gmail.com>
2023-12-31 20:51:10 +0900

    [level 1] Title: 공원 산책, Time: 0.29 ms, Memory: 33.7 MB -BaekjoonHub

--- 프로그래머스/1/172928. 공원 산책/공원 산책.js
+++ 프로그래머스/1/172928. 공원 산책/공원 산책.js
@@ -1,20 +1,14 @@
-// 1. 동, 서, 남, 북에 따른 좌표 생성
-// 2. park_x, park_y 최대 좌표 구하기
-// 3. 시작점 위치 구하기
-// 4. routes에 따른 거리 구하기
-// 5. 조건이 충족할 때만 위치 업데이트 
-
 function solution(park, routes) {
-    const direction = { 
-        'N': [-1, 0],
-        'S': [1, 0],
-        'W': [0, -1],
-        'E': [0, 1]
+    const moveDirections = { 
+        'N': [-1, 0], // 북쪽: 위로 이동
+        'S': [1, 0],  // 남쪽: 아래로 이동
+        'W': [0, -1], // 서쪽: 왼쪽으로 이동
+        'E': [0, 1]   // 동쪽: 오른쪽으로 이동
     };
     let start = [];
     
-    const park_x_xax = park.length -1;
-    const park_y_max = park[0].length -1;
+    const maxRowIndex = park.length - 1;
+    const maxColumnIndex = park[0].length - 1;
 
     for (let i = 0; i < park.length; i++) {
         for (let j = 0; j < park[i].length; j++) {
@@ -26,23 +20,22 @@ function solution(park, routes) {
 
     for (let i = 0; i < routes.length; i++) {
         const [dir, move] = routes[i].split(' ');
-        let [nx, ny] = [start[0], start[1]];
+        let [currentRow, currentColumn] = [start[0], start[1]];
         let validMove = true;
         
         for(let j = 0; j < Number(move); j++) {
-            nx += direction[dir][0];
-            ny += direction[dir][1];
+            currentRow += moveDirections[dir][0];
+            currentColumn += moveDirections[dir][1];
             
-            if (nx < 0 || nx > park_x_xax || ny < 0 || ny > park_y_max || park[nx][ny] == 'X') {
+            if (currentRow < 0 || currentRow > maxRowIndex || currentColumn < 0 || currentColumn > maxColumnIndex || park[currentRow][currentColumn] == 'X') {
                 validMove = false;
                 break;
             }
         }
         
         if(validMove) {
-            start = [nx, ny];
+            start = [currentRow, currentColumn];
         }
-
     }
 
     return start;
