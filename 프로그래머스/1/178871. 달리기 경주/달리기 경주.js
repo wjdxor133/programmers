e32db1a06b448f23fe06334d54c289c8d6aa1789
Woosung Lim <wjdxor1224@gmail.com>
2024-01-05 16:53:59 +0900

    [level 1] Title: 달리기 경주, Time: 0.26 ms, Memory: 33.6 MB -BaekjoonHub

--- 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
+++ 프로그래머스/1/178871. 달리기 경주/달리기 경주.js
@@ -2,26 +2,25 @@
 // 선수의 이름을 부를 때 마다, 등수를 바꿔줘야 함
 
 function solution(players, callings) {
-    const playerRanking = {};
-    players.forEach((player, index) => {
-        if (!playerRanking[player]) {
-            playerRanking[player] = index;
+    const playerRanking = players.reduce((obj, player, index) => {
+        if (!obj[player]) {
+            obj[player] = index;
         }
-    });
-
-    let sortedPlayers = players.slice().sort((a, b) => playerRanking[a] - playerRanking[b]);
+        return obj;
+    }, {});
 
     callings.forEach((player) => {
         const playerIndex = playerRanking[player];
         if (playerIndex > 0) {
-            const temp = sortedPlayers[playerIndex - 1];
-            sortedPlayers[playerIndex - 1] = sortedPlayers[playerIndex];
-            sortedPlayers[playerIndex] = temp;
+            const temp = players[playerIndex - 1];
+            players[playerIndex - 1] = player;
+            players[playerIndex] = temp;
             playerRanking[temp] = playerIndex;
             playerRanking[player] = playerIndex - 1;
         }
     });
 
-    return sortedPlayers;
+    return players;
 }
+
 
