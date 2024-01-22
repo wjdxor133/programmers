100616ce73e7277bde5dc681a8c41a844d7562e0
Woosung Lim <wjdxor1224@gmail.com>
2024-01-22 21:58:32 +0900

    [level 0] Title: 짝수의 합, Time: 0.04 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/0/120831. 짝수의 합/짝수의 합.js
@@ -0,0 +1,11 @@
+function solution(n) {
+    var answer = 0;
+    
+    for(let i = 0; i <= n; i++) {
+        if(i % 2 === 0) {
+            answer += i;
+        }
+    }
+    
+    return answer;
+}
